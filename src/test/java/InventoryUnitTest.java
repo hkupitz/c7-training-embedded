a5632ba253185fb6e7a6d6eb703868bbdb6702c7
Hendrik Kupitz <hendrik.kupitz@camunda.com>
2023-12-05 14:30:39 +0100

    Adjust test demo

--- src/test/java/InventoryUnitTest.java
+++ src/test/java/InventoryUnitTest.java
@@ -1,34 +1,31 @@
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.assertThat;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.findId;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.runtimeService;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
-import org.camunda.community.process_test_coverage.junit5.platform7.ProcessEngineCoverageExtension;
+import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@Deployment(resources = {"inventory-process.bpmn"})
 @ExtendWith(ProcessEngineCoverageExtension.class)
+@Deployment(resources = "inventory-process.bpmn")
 public class InventoryUnitTest {
-
-  public static final String INVENTORY_PROCESS_ID = "InventroyProcess";
-
-  public static final String CHECK_AVAILABILITY_TASK = "Check availability";
-  public static final String RESERVE_AVAILABLE_ITEMS_TASK = "Reserve available items";
-  public static final String REMOVE_UNAVAILABLE_ITEMS_TASK = "Remove unavailable items";
-  public static final String INVENTORY_COMPLETED_EVENT = "Inventory completed";
-
-  public static final String AVAILABILITY_CHECK_TOPIC = "availabilityChek";
-  public static final String ITEM_REMOVAL_TOPIC = "itemRemoval";
-  public static final String ITEM_RESERVATION_TOPIC = "itemReservation";
-
-  public static final String ORDER_ITEMS_NUM_VAR = "orderItemsNum";
-  public static final String AVAILABLE_ITEMS_NUM_VAR = "availableItemsNu";
 
   @Test
   public void testHappyPath() {
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("InventoryProcess");
+    Map<String, Object> variables = new HashMap<>();
+    variables.put("orderItemsNum", 20);
+    variables.put("availableItemsNum", 20);
+
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey(
+      "InventoryProcess", variables);
 
     assertThat(processInstance).isStarted();
+    assertThat(processInstance).isEnded()
+      .hasPassed(findId("Check availability"))
+      .hasPassed(findId("Reserve available items"));
   }
 }
