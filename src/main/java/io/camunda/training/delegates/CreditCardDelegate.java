8b92f0ad6be2b10565d1f436deb1d34cbbe3f442
Hendrik Kupitz <hendrik.kupitz@camunda.com>
2023-12-07 11:25:12 +0100

    Add exercise 10

--- src/main/java/io/camunda/training/delegates/CreditCardDelegate.java
+++ src/main/java/io/camunda/training/delegates/CreditCardDelegate.java
@@ -1,2 +1,31 @@
-package io.camunda.training.delegates;public class CreditCardDelegate {
+package io.camunda.training.delegates;
+
+import io.camunda.training.services.CreditCardService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("creditCardDelegate")
+public class CreditCardDelegate implements JavaDelegate {
+
+  private CreditCardService service;
+
+  public CreditCardDelegate(@Autowired CreditCardService service) {
+    this.service = service;
+  }
+
+  @Override
+  public void execute(DelegateExecution execution) throws Exception {
+    String cardNumber = (String) execution.getVariable("cardNumber");
+    String expiryDate = (String) execution.getVariable("expiryDate");
+    String cvc = (String) execution.getVariable("cvc");
+    Double openAmount = (Double) execution.getVariable("openAmount");
+
+    if (service.validateExpiryDate(expiryDate)) {
+      service.chargeAmount(cardNumber, cvc, expiryDate, openAmount);
+    } else {
+      throw new IllegalArgumentException("Invalid expiry date: " + expiryDate);
+    }
+  }
 }
--- src/test/java/ProcessTests.java
+++ src/test/java/ProcessTests.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.Test;
 
+
 public class ProcessTests {
 
   @Test
   public void testHappyPath() {
+
+    run
 
     // Implement testing logic
   }
