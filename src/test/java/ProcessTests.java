e5f8b66d48102cc5ba2110290744e0717e703a2a
hkupitz <hendrik.kupitz@alumni.fh-aachen.de>
2025-11-14 17:49:33 +0000

    Update solution

--- src/test/java/ProcessTests.java
+++ src/test/java/ProcessTests.java
@@ -1,13 +1,14 @@
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.HashMap;
-import java.util.Map;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.community.process_test_coverage.junit5.platform7.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.assertj.core.api.Assertions.*;
 
 @Deployment(resources = "payment.bpmn")
 @ExtendWith(ProcessEngineCoverageExtension.class)
@@ -16,14 +17,13 @@ public class ProcessTests {
   @Test
   public void testCreditCardPath() {
 
-    // Create a HashMap to put in variables for the process instance
+    // Create a HashMap for the variables payload
     Map<String, Object> variables = new HashMap<>();
     variables.put("orderTotal", 30.00);
     variables.put("customerCredit", 20.00);
 
-    // Start process with Java API and variables
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("PaymentProcess",
-      variables);
+    // Start process via Java API
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("PaymentProcess", variables);
 
     // Make assertions on the process instance
     assertThat(processInstance).isEnded().hasPassed(findId("Charge credit card"));
