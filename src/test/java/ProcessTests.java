f3ba2e745e2622b7b8ae3ec221a8409ccdcbcf05
hkupitz <hendrik.kupitz@alumni.fh-aachen.de>
2025-12-03 21:42:01 +0100

    Update solution

--- src/test/java/ProcessTests.java
+++ src/test/java/ProcessTests.java
@@ -78,7 +78,7 @@ public class ProcessTests {
 
   @Test
   public void testInvalidExpiryDate(){
-    Mocks.register("paymentCompletion", (JavaDelegate) execution -> {});
+    Mocks.register("completePayment", (JavaDelegate) execution -> {});
 
     Map<String, Object> variables = new HashMap<String, Object>();
     variables.put("orderTotal", 30.00);
